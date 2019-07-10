8d8aa09d75a8eea4132b5762eab53dd23466d5f9
Saul Ivan Rivas Vega <saul.ivan.rivas.vega@gmail.com>
2019-07-10 09:51:40 -0500

    Deleted testing branch

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -14,7 +14,7 @@ const logger = createLogger();
 const rootReducer = combineReducers({searchRobots, requestRobots});
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
-ReactDOM.render(<Provider store={store}>
+ReactDOM.render(<Provider store={store}> 
                     <App />
                 </Provider>, document.getElementById('root'));
 
